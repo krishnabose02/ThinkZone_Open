d590cb8608ebe44b14c57caa113fa869794f570f
krishna <krishna.bose02@gmail.com>
2019-08-08 03:32:54 +0530

    Fixing attendance page

--- src/app/pages/attendance/attendance.page.ts
+++ src/app/pages/attendance/attendance.page.ts
@@ -23,7 +23,7 @@ export class AttendancePage {
   attendance_day = '';
   max_date = '';
   min_date = '';
-  is_attendance_taken = true;
+  is_attendance_taken = false;
 
   _userid: string;
   _username: string;
@@ -34,6 +34,8 @@ export class AttendancePage {
   month: string;
   months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   clickState = 0;
+  holiday_name: any;
+  error_message: string;
 
   @ViewChild('datePicker') datePicker: IonDatetime;
   constructor(
@@ -68,7 +70,7 @@ export class AttendancePage {
         if (res.length > 0) {
           this.is_attendance_taken = true;
         } else {
-          this.is_attendance_taken = true;
+          this.is_attendance_taken = false;
         }
         console.log('@@@Attendance taken: ' + this.is_attendance_taken);
         loading.dismiss();
@@ -191,14 +193,58 @@ export class AttendancePage {
         this.takeattendance_onclick();
       } else {
         this.clickState = 0;
+        this.holiday_name = '';
+        this.error_message = '';
       }
       return;
     } else if (buttonClicked === 'holiday') {
       if (this.clickState === 1) {
-        this.setholiday_onclick();
+        // this.setholiday_onclick();
+        this.setholiday();
       } else {
         this.clickState = 1;
       }
     }
+  }
+
+  // Copied from Holiday modal
+  async setholiday() {
+    if (this.holiday_name == null || this.holiday_name.trim() === '') {
+      this.error_message = 'Please enter a holiday name to continue';
+      return;
+    }
+
+    const data = {
+      isholiday : true,
+      holidayname : this.holiday_name,
+      availability : null,
+      userid : this._userid,
+      username : this._username,
+      centerid : null,
+      centername : null,
+      attendancedate : this.attendance_date,
+      attendanceday : this.attendance_day,
+      studentid : null,
+      studentname : null,
+      program: null
+    };
+    this.save(data);
+  }
+
+  async save(data) {
+    this.attendance_list = [];
+    this.attendance_list.push(data);
+    const loading = await this.loadingController.create({});
+      await loading.present();
+      await this.api.saveattendance(this.attendance_list)
+        .subscribe(res => {
+          // console.log('@@@all student list: ' + JSON.stringify(res));
+          this.showAlert('Info', '', 'Attendance saved ' + res['status'] + ' !!!');
+          loading.dismiss();
+          this.modalController.dismiss({data: 'Ok'});
+        }, err => {
+          console.log(err);
+          loading.dismiss();
+        });
   }
 }
